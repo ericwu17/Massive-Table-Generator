5bf8f67ffaf41826a12497e0c0f984ea8ef12352
Eric <ericwu17@protonmail.com>
2025-05-28 13:44:27 -0700

    add the massive table in binary form

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ use std::{
 };
 
 use cube::{
-    CUBE_SOLVED_STATE, NUM_MOVES, apply_move, decode_cube_state, decode_pretty_print_moves,
-    encode_cube_state, invert_moves,
+    CUBE_SOLVED_STATE, NUM_MOVES, NUM_STATES, apply_move, decode_cube_state,
+    decode_pretty_print_moves, encode_cube_state, invert_moves,
 };
 
 fn main() {
@@ -40,20 +40,27 @@ fn main() {
         }
     }
 
+    println!(
+        "Finished exploring all the states. Total number of states is {}",
+        seen_states.len()
+    );
+
     let mut massive_table: Vec<([u8; 14], Vec<u8>)> = Vec::new();
 
     for (state, solution) in seen_states.into_iter() {
         massive_table.push((decode_cube_state(state), invert_moves(solution)));
     }
 
-    massive_table.sort_by_key(|(_, sol)| sol.len());
+    println!("Inserted all inverses into a massive table");
 
-    save_massive_table_to_file(&massive_table);
+    save_massive_table_to_files(massive_table);
 
-    println!("done!");
+    println!("Done!");
 }
 
-fn save_massive_table_to_file(table: &Vec<([u8; 14], Vec<u8>)>) {
+fn save_massive_table_to_files(mut table: Vec<([u8; 14], Vec<u8>)>) {
+    table.sort_by_key(|(_, sol)| sol.len());
+
     let file = File::create("massive_table.txt").unwrap();
     let mut file_buf_writer = BufWriter::new(file);
 
@@ -67,4 +74,47 @@ fn save_massive_table_to_file(table: &Vec<([u8; 14], Vec<u8>)>) {
         file_buf_writer.write(sol.as_bytes()).unwrap();
         file_buf_writer.write("\n".as_bytes()).unwrap();
     }
+
+    let mut solutions: Vec<Option<Vec<u8>>> = vec![None; NUM_STATES as usize];
+    assert_eq!(table.len(), solutions.len());
+    for (state, sol) in table.clone().into_iter() {
+        let state_idx = encode_cube_state(&state) as usize;
+        let sol_bytes = encode_solution_as_bytes(sol);
+        assert!(solutions[state_idx].is_none());
+        solutions[state_idx] = Some(sol_bytes);
+    }
+
+    let binary_file = File::create("massive_table.bin").unwrap();
+    let mut bin_file_buf_writer = BufWriter::new(binary_file);
+
+    for idx in 0..solutions.len() {
+        let sol = solutions[idx].as_deref().unwrap();
+        assert!(sol.len() == 6);
+        bin_file_buf_writer.write(sol).unwrap();
+    }
+}
+
+fn encode_solution_as_bytes(sol: Vec<u8>) -> Vec<u8> {
+    // encode the solution as a 6 byte chunk for the binary lookup table
+    assert!(sol.len() <= 12);
+
+    let mut sol_padded: Vec<u8> = sol;
+
+    while sol_padded.len() < 12 {
+        sol_padded.push(15); // use 15 = 0b1111 as a marker for an empty move.
+    }
+    assert!(sol_padded.len() == 12);
+
+    let mut result = Vec::new();
+
+    for i in 0..6 {
+        let first = sol_padded[2 * i];
+        let second = sol_padded[2 * i + 1];
+
+        let combined = first + (second << 4);
+
+        result.push(combined)
+    }
+
+    result
 }
